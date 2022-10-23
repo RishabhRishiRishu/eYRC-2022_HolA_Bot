02207cd8b33209114d271689da6262b3736584d9
Rishabh Dugar <rishabhrishirishu@gmail.com>
2022-10-23 14:45:12 -0700

    .

--- eyrc-2022_holabot/scripts/task_0.py
+++ eyrc-2022_holabot/scripts/task_0.py
@@ -17,67 +17,134 @@
 *****************************************************************************************
 '''
 
-# Team ID:			[ Team-ID ]
-# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Team ID:			[ hb_2563 ]
+# Author List:		[ Rishabh, Arvind, Jordan, Mukul ]
 # Filename:			task_0.py
-# Functions:
-# 					[ Comma separated list of functions in this file ]
-# Nodes:		    Add your publishing and subscribing node
+# Functions:        [ callback, main ]
+# Nodes:		    [ /turtle1/cmd_vel, /turtle1/pose]
 
 
 ####################### IMPORT MODULES #######################
 import sys
 import traceback
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 ##############################################################
 
 
-def callback(data):
-	"""
-	Purpose:
-	---
-	This function should be used as a callback. Refer Example #1: Pub-Sub with Custom Message in the Learning Resources Section of the Learning Resources.
+def callback(data:Pose):
+    """
+    Purpose:
+    ---
+    This function should be used as a callback. Refer Example #1: Pub-Sub with Custom Message in the Learning Resources Section of the Learning Resources.
     You can write your logic here.
     NOTE: Radius value should be 1. Refer expected output in document and make sure that the turtle traces "same" path.
 
-	Input Arguments:
-	---
+    Input Arguments:
+    ---
         `data`  : []
             data received by the call back function
 
-	Returns:
-	---
+    Returns:
+    ---
         May vary depending on your logic.
 
-	Example call:
-	---
+    Example call:
+    ---
         Depends on the usage of the function.
-	"""
+    """
+    global STAGE, START_POINT, VEL_MSG
+    if STAGE==-1: # first callback
+        STAGE = 0
+        START_POINT = (data.x, data.y) # saves start point
+    elif STAGE==0 and data.theta>3.13: # Reached end point of arc
+        STAGE = 1
+    elif STAGE==1 and data.theta>=(-1.57): # Made the turn towards start
+        STAGE = 2
+    elif STAGE==2 and data.y<START_POINT[1]: # Crossed the initial point
+        STAGE = 3
+        print('Done!!')
+
+    # change speeds
+    if STAGE==-1:
+        pass
+    elif STAGE==0:
+        VEL_MSG.linear.x = 1
+        VEL_MSG.angular.z = 1
+        print('My turtleBot is: Moving in circle!!')
+    elif STAGE==1:
+        VEL_MSG.linear.x = 0
+        VEL_MSG.angular.z = 1
+        print('My turtleBot is: Rotating!')
+    elif STAGE==2:
+        VEL_MSG.linear.x = 1
+        VEL_MSG.angular.z = 0
+        print('My turtleBot is: Moving Straight!!!')
+    elif STAGE==3:
+        VEL_MSG.linear.x = 0
+        VEL_MSG.angular.z = 0
+        velocity_publisher.publish(VEL_MSG)
+        STAGE=4
+    velocity_publisher.publish(VEL_MSG)
+    if STAGE<4:
+        print(f"{data.theta:.2f}")
+    return(1)
 
 
 def main():
-	"""
-	Purpose:
-	---
-	This function will be called by the default main function given below.
+    """
+    Purpose:
+    ---
+    This function will be called by the default main function given below.
     You can write your logic here.
 
-	Input Arguments:
-	---
+    Input Arguments:
+    ---
         None
 
-	Returns:
-	---
+    Returns:
+    ---
         None
 
-	Example call:
-	---
+    Example call:
+    ---
         main()
-	"""
+    """
+    global STAGE, velocity_publisher, VEL_MSG
+
+    # setup Publisher and subscriber
+    rospy.init_node('turtle_revolve', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.Subscriber("/turtle1/pose", Pose, callback)
+    rate = rospy.Rate(1000)
+
+
+    # make twist object
+    VEL_MSG = Twist()
+    VEL_MSG.angular.x = 0
+    VEL_MSG.angular.y = 0
+    VEL_MSG.angular.z = 0
+    VEL_MSG.linear.x = 0
+    VEL_MSG.linear.y = 0
+    VEL_MSG.linear.z = 0
+    
+
+    # main logic
+    STAGE = -1
+    while not rospy.is_shutdown():
+        if STAGE==4:
+            break
+        rate.sleep()
 
 
 ################# ADD GLOBAL VARIABLES HERE #################
-
-
+STAGE = -1
+PI = 3.14159
+START_POINT = None
+VEL_MSG = None
+velocity_publisher = None
+# PREV_DATA = 0
 
 ##############################################################
 
